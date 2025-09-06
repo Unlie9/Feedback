6def5ffc2750e239f784741e291df89b66b1cbb5
unlie9 <unlie.devops9@gmail.com>
2025-09-06 22:05:41 +0300

    black refactoring

--- app/backend/django/config/urls.py
+++ app/backend/django/config/urls.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-  path('admin/', admin.site.urls), 
-  path("api/", include("config.api.urls")),
-  
-  # path('api/feedbacks/', include('apps.feedback.urls'))
+    path('admin/', admin.site.urls),
+    path("api/", include("config.api.urls")),
+    # path('api/feedbacks/', include('apps.feedback.urls'))
 ]
