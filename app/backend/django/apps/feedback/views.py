3d985aea3c724f3e2e045e8b3b2da536f6d1e3bc
unlie9 <unlie.devops9@gmail.com>
2025-09-06 19:39:07 +0300

    added django jazminn

--- app/backend/django/apps/feedback/views.py
+++ app/backend/django/apps/feedback/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from rest_framework import generics
+
+from apps.feedback.models import Feedback
+from apps.feedback.serializers import FeedbackListSerializer
+
+
+class FeedbackListView(generics.ListAPIView):
+    queryset = Feedback.objects.all()
+    serializer_class = FeedbackListSerializer
--- app/backend/django/config/settings.py
+++ app/backend/django/config/settings.py
@@ -9,6 +9,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -83,3 +84,120 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+JAZZMIN_UI_TWEAKS = {
+    "theme": "pulse",
+    "navbar": "navbar-indigo navbar-dark",
+    "sidebar": "sidebar-dark-warning",
+    "brand_colour": "navbar-indigo",
+}
+
+JAZZMIN_SETTINGS = {
+    # NOTE title of the window (Will default to current_admin_site.site_title if absent or None)
+    "site_title": "Feedback Admin",
+    # NOTE Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_header": "Feedback",
+    # NOTE Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_brand": "Feedback",
+    # NOTE Logo to use for your site, must be present in static files, used for brand on top left
+    # "site_logo": "books/img/logo.png",
+    # NOTE Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
+    "login_logo": None,
+    # NOTE Logo to use for login form in dark themes (defaults to login_logo)
+    "login_logo_dark": None,
+    # NOTE CSS classes that are applied to the logo above
+    "site_logo_classes": "img-circle",
+    # NOTE Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
+    "site_icon": None,
+    # NOTE Welcome text on the login screen
+    "welcome_sign": "Welcome to the Feedback",
+    # NOTE Copyright on the footer
+    "copyright": "Acme Feedback Ltd",
+    # NOTE List of model admins to search from the search bar, search bar omitted if excluded
+    # If you want to use a single search field you dont need to use a list, you can use a simple string
+    "search_model": ["auth.User", "auth.Group"],
+    # NOTE Field name on user model that contains avatar ImageField/URLField/Charfield or a callable that receives the user
+    "user_avatar": None,
+    ############
+    # Top Menu #
+    ############
+    # NOTE Links to put along the top menu
+    "topmenu_links": [
+        # NOTE Url that gets reversed (Permissions can be added)
+        {"name": "Home", "url": "admin:index", "permissions": ["auth.view_user"]},
+        # NOTE model admin to link to (Permissions checked against model)
+        {"model": "auth.User"},
+    ],
+    #############
+    # User Menu #
+    #############
+    # NOTE Additional links to include in the user menu on the top right ("app" url type is not allowed)
+    "usermenu_links": [
+        {"name": "Support", "url": "https://github.com/farridav/django-jazzmin/issues", "new_window": True},
+        {"model": "auth.user"},
+    ],
+    #############
+    # Side Menu #
+    #############
+    # NOTE Whether to display the side menu
+    "show_sidebar": True,
+    # NOTE Whether to aut expand the menu
+    "navigation_expanded": True,
+    # NOTE Hide these apps when generating side menu e.g (auth)
+    "hide_apps": [],
+    # NOTE Hide these models when generating side menu (e.g auth.user)
+    "hide_models": [],
+    # NOTE List of apps (and/or models) to base side menu ordering off of (does not need to contain all apps/models)
+    "order_with_respect_to": ["auth", "books", "books.author", "books.book"],
+    # NOTE Custom links to append to app groups, keyed on app name
+    "custom_links": {
+        "books": [
+            {
+                "name": "Make Messages",
+                "url": "make_messages",
+                "icon": "fas fa-comments",
+                "permissions": ["books.view_book"],
+            }
+        ]
+    },
+    # NOTE Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
+    # for the full list of 5.13.0 free icon classes
+    "icons": {
+        "auth": "fas fa-users-cog",
+        "auth.user": "fas fa-user",
+        "auth.Group": "fas fa-users",
+    },
+    # NOTE Icons that are used when one is not manually specified
+    "default_icon_parents": "fas fa-chevron-circle-right",
+    "default_icon_children": "fas fa-circle",
+    #################
+    # Related Modal #
+    #################
+    # NOTE Use modals instead of popups
+    "related_modal_active": False,
+    #############
+    # NOTE UI Tweaks #
+    #############
+    # NOTE Relative paths to custom CSS/JS scripts (must be present in static files)
+    "custom_css": None,
+    "custom_js": None,
+    #  NOTE Whether to link font from fonts.googleapis.com (use custom_css to supply font otherwise)
+    "use_google_fonts_cdn": True,
+    # NOTE Whether to show the UI customizer on the sidebar
+    "show_ui_builder": True,
+    ###############
+    # Change view #
+    ###############
+    # NOTE Render out the change view as a single form, or in tabs, current options are
+    # - single
+    # - horizontal_tabs (default)
+    # - vertical_tabs
+    # - collapsible
+    # - carousel
+    # "changeform_format": "horizontal_tabs",
+    # # override change forms on a per modeladmin basis
+    # "changeform_format_overrides": {"auth.user": "collapsible", "auth.group": "vertical_tabs"},
+    # # Add a language dropdown into the admin
+    # "language_chooser": True,
+}
