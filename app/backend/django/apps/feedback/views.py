4c254ecba68b9466449d859e782609a5707cfddf
unlie9 <unlie.devops9@gmail.com>
2025-09-07 20:37:09 +0300

    added secrets

--- app/backend/django/apps/feedback/views.py
+++ app/backend/django/apps/feedback/views.py
@@ -1,6 +1,7 @@
+from rest_framework import generics
+
 from apps.feedback.models import Feedback
 from apps.feedback.serializers import FeedbackListSerializer
-from rest_framework import generics
 
 
 class FeedbackListView(generics.ListAPIView):
