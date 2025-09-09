68642d680a781001364fe44607dd5cff87b228b4
unlie9 <unlie.devops9@gmail.com>
2025-09-09 22:53:58 +0300

    added api logging

---
+++ app/backend/django/apps/base/middleware.py
@@ -0,0 +1,24 @@
+import logging
+
+info_logger = logging.getLogger("info")
+error_logger = logging.getLogger("error")
+
+
+class LoggingMiddleware:
+
+    def __init__(self, get_response) -> None:
+        self._get_response = get_response
+
+    def __call__(self, request):
+
+        info_logger.info(msg="Request", extra={"request": request.path})
+        try:
+            response = self._get_response(request)
+        except Exception as error:
+            error_logger.error(
+                f"Error in get response: ({error})", extra={"request": request.path}
+            )
+        else:
+            info_logger.info("Response", extra={"request": response.status_code})
+
+        return response
--- app/backend/django/apps/feedback/views.py
+++ app/backend/django/apps/feedback/views.py
@@ -1,9 +1,25 @@
+import os
+
 from rest_framework import generics
 
 from apps.feedback.models import Feedback
 from apps.feedback.serializers import FeedbackListSerializer
+from django.conf import settings
 
 
 class FeedbackListView(generics.ListAPIView):
     queryset = Feedback.objects.all()
     serializer_class = FeedbackListSerializer
+
+    def get(self, request, *args, **kwargs):
+        print("TEST")
+        print(settings.BASE_DIR)
+
+        path1 = os.path.join("my_directory", "my_file.txt")
+        print(path1)
+
+        log_dir = os.path.join(settings.BASE_DIR, "logs", "warning.log")
+
+        print(log_dir)
+
+        return self.list(request, *args, **kwargs)
